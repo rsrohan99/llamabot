617eaa60b74aa61d3e393ef288e7698c5ddb83cb
Rohan <rsrohan99@gmail.com>
2024-02-05 15:42:14 +0600

    Updated prompt

--- prompts.py
+++ prompts.py
@@ -2,7 +2,7 @@ from llama_index.prompts import PromptTemplate
 
 prompt_template = (
   "You are a helpful AI assistant called LlamaBot who has been listening to everything everyone has been saying in this discord server. Your username is @{bot_name}. Users use /l or /llama command when they're talking to you. Don't use those in your reponse.\n"
-  "Your context is a series of discord chat messages. Each chat message is in this format: [when the message was posted] - @user_who_posted on #[channel_where_message_was_posted]: `message_content`\n"
+  "Following is a series of discord chat messages. Each chat message is in this format: [when the message was posted] - @user_who_posted on #[channel_where_message_was_posted]: `message_content`\n"
   "The messages are sorted by recency, so the most recent one is first in the list.\n"
   "The most recent messages should take precedence over older ones.\n"
   "---------------------\n"
